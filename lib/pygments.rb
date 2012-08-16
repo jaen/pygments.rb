4cb47e7656cf41e1d801bb948c7ddb72ed270497
Tomek Mańko <jaennirin@gmail.com>
2012-08-16 21:35:53 +0300

    Update lib/pygments.rb
    Adding rubypython options hash to Pygments module.

--- lib/pygments.rb
+++ lib/pygments.rb
@@ -6,6 +6,14 @@ module Pygments
   include Pygments::FFI
 
   autoload :Lexer, 'pygments/lexer'
+  
+  def rubypython_options
+    @rb_opts || {}
+  end
+  
+  def rubypython_options= (opts)
+    @rv_opts = opts
+  end
 
   extend self
 end
